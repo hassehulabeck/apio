bc899f36a134f236323c14b56230925a035eaba6
hassehulabeck <hansbudandersson@gmail.com>
2021-02-05 09:24:16 +0100

    Added a lot

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="app">
-    <List />
+    <List :products="products" />
   </div>
 </template>
 
 <script>
 import List from "./components/List";
+import axios from "axios";
+
 export default {
   name: "App",
   components: {
     List,
+  },
+  data() {
+    return {
+      url: "https://www.hulabeck.se/hdtml/temp/products.json",
+      // Kontrollera alltid din datakälla, helst visuellt.
+      products: null,
+    };
+  },
+  mounted() {
+    // När ska du hämta data?
+    axios
+      .get(this.url)
+      .then((response) => {
+        this.products = response.data.products;
+      })
+      .catch((error) => {
+        console.error("Fel:" + error);
+      })
+      .then(() => {
+        console.log("Körs alltid");
+      });
   },
 };
 </script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>Hej</div>
+  <div>
+    <h1>Produkter</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }} ({{ product.consumerPrice }})
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "List",
+  props: {
+    products: Array,
+  },
 };
 </script>
 
